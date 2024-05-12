7a0eddc04ae47e3cec3464f3521506fd283164d8
Dochanii2 <dmlwhd01011@gmail.com>
2024-05-12 17:10:08 +0900

    [ADD] onRemove

--- react study/hello-react/src/Sample.js
+++ react study/hello-react/src/Sample.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Sample = () => {
   const [names, setNames] = useState([]);
   const [inputText, setInputText] = useState("");
-  const [nextId, setNextId] = useState(5);
+  const [nextId, setNextId] = useState(1);
 
   const onChange = (e) => {
     setInputText(e.target.value);
@@ -13,24 +13,35 @@ const Sample = () => {
       id: nextId,
       text: inputText,
     });
-    setNames(nextNames);
     setNextId(nextId + 1);
+    setNames(nextNames);
     setInputText("");
   };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
   const onRemove = (id) => {
     const nextNames = names.filter((name) => name.id !== id);
     setNames(nextNames);
   };
+
   const nameList = names.map((name) => (
     <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
       {name.text}
     </li>
   ));
+
   return (
     <div>
-      <ul>{nameList}</ul>;
+      <ul>{nameList}</ul>
       <input value={inputText} onChange={onChange} />
-      <button onClick={onClick}>추가</button>
+      <button onClick={onClick} onKeyPress={onKeyPress}>
+        추가
+      </button>
     </div>
   );
 };
